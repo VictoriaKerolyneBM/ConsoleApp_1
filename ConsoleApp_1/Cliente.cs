fc6bb1e7d800e5d28d3834892d895da657ad435e
VictoriaKerolyneBM <victoria.kbmedeiros@gmail.com>
2023-03-08 15:58:19 -0300

    criação da classe endereço juntamente com o método construtor, os get e set.

--- ConsoleApp_1/Cliente.cs
+++ ConsoleApp_1/Cliente.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp_1
 {
-    internal class Cliente
+    public class Cliente
     {
+        private int Id;
+        private string Nome;
+        private string Telefone;
+
+        public Cliente(int id, string nome, string telefone) 
+        {
+            Id = id;
+            Nome = nome;
+            Telefone = telefone;
+
+        }
+        public int getId()
+        {
+            return Id;
+        }
+
+        public string getNome()
+        {
+            return Nome;
+        }
+
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+
+        public void setNome(string nome)
+        {
+            Nome=nome;
+        }
+
+        public void setId(int id)
+        {
+            Id = id;
+        }
+
+        public void setTelefone(string telefone)
+        {
+            Telefone=telefone;
+
+        }
+
     }
 }
---
+++ ConsoleApp_1/Endereco.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_1
+{
+    public class Endereco
+    {
+        private int Id;
+        private int Cep;
+        private string Logradouro;
+        private int Numero;
+        private string Bairro;
+        private string Cidade;
+
+     public Endereco(int id,int cep, string logradouro,int numero,string bairro, string cidade) 
+        {
+            Id = id;
+            Cep = cep;
+            Logradouro = logradouro;
+            Numero = numero;
+            Bairro = bairro;
+            Cidade = cidade;
+        }
+
+        public int getId()
+        {
+            return Id;
+        }
+
+        public int getCep()
+        {
+            return Cep;
+        }
+
+        public string getLogradouro()
+        {
+            return Logradouro;
+        }
+        public int getNumero()
+        {
+            return Numero;
+        }
+
+        public string getBairro()
+        {
+            return Bairro;
+        }
+
+        public string getCidade()
+        {
+            return Cidade;
+        }
+
+        public void setId(int id)
+        {
+           Id=id;
+        }
+
+        public void setCep(int cep)
+        {
+            Cep=cep;
+        }
+
+        public void setLogradouro(string logradouro)
+        {
+             Logradouro = logradouro;
+        }
+        public void setNumero(int numero)
+        {
+            Numero = numero ;
+        }
+
+        public void setBairro(string bairro)
+        {
+             Bairro=bairro;
+        }
+
+        public void setCidade(string cidade)
+        {
+             Cidade=cidade;
+        }
+    }
+}
