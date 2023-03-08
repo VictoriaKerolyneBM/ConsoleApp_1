04a24e1d617d8d2c6a796a8ceb036def9e18d86b
VictoriaKerolyneBM <victoria.kbmedeiros@gmail.com>
2023-03-08 14:24:08 -0300

    Criei os atributos e o método construtor

---
+++ ConsoleApp_1/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_1
+{
+    public class Produto
+    {
+        private int Id;
+        private string Descricao;
+        private decimal Valor;
+
+        public Produto(int id,string descricao, decimal valor) 
+        {
+            Id =id;
+            Descricao=descricao;
+            Valor = valor;
+        
+        }
+
+    }
+}
